d9919b5e76eba6d1a29803bf6c73038274ef486e
ziggy14741 <zavisgreat@gmail.com>
2025-02-22 13:49:58 -0500

    finished player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2D;
 
+    private float _xSpeed;
+    private float _ySpeed;
+    public float speed = 20;
+    private string _ImputX = "Horizontal";
+    private string _ImputY = "Vertical";
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+         
+    }
+
+    private void Update()
+    {
+        _xSpeed = Input.GetAxis(_ImputX);
+        _ySpeed = Input.GetAxis(_ImputY);
+
+        rigidbody2D.velocity = new Vector2(_xSpeed, _ySpeed)* 5;
+    }
 }
--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
+private Camera _camera;
 
+    private string _Cameraname = "Game_Camera";
+
+    private Vector3 _positionMouse;
+
+    private void Start()
+    {
+        _camera = GameObject.Find(_Cameraname).GetComponent<Camera>();
+
+    }
+    private void Update()
+    {
+        _positionMouse = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 pos = _positionMouse - transform.position;
+
+        float rotZ = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ - 90);
+    }
 }
