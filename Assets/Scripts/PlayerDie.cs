501868ad01478fcd8c714de66e803f570aadabb0
ziggy14741 <zavisgreat@gmail.com>
2025-03-15 14:00:13 -0400

    70% worth complete
    movement, shooting, and death are complete

--- Assets/Scripts/PlayerDie.cs
+++ Assets/Scripts/PlayerDie.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class PlayerDie : MonoBehaviour
 {
+    public GameObject endpanel;
+    private string Enemy = "Enemy";
 
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == Enemy) 
+        {
+            endpanel . SetActive(true);
+            gameObject .SetActive(false);
+        }
+    }
+    
 }
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -1,6 +1,51 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    public GameObject preFab;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
+
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
+
+
+    private void Update()
+    { 
+      TimerMethod();
+        Shoot();
+    }
+
+
+    private void TimerMethod()
+    {
+        if (!_canShoot)
+        {
+            _currentTime -= Time.deltaTime;
+
+            if (_currentTime < 0)
+            {
+                _canShoot = true;
+                _currentTime = Timer;
+
+            }
+        }
+    }
+    
+
+
+    private void Shoot()
+    {
+  if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        {
+            GameObject bullet = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
+            bullet.transform.SetParent(bulletTrash);
+            _canShoot = false;
+        }
+
+    }
 
 }
+ 
